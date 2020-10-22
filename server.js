b20998c22a49d9c6ba9cc1884eb72a90d823a27b
John Alonso <jvalon9455@gmail.com>
2020-10-21 21:38:32 -0400

    added models folder with index and workout

--- server.js
+++ server.js
@@ -11,30 +11,30 @@ app.use(express.json());
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/Workout-Tracker",
     {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
     }
-  );
+);
 
-  const connection = mongoose.connection;
+const connection = mongoose.connection;
 
 connection.on("connected", () => {
-  console.log("Mongoose successfully connected.");
+    console.log("Mongoose successfully connected.");
 });
 
 connection.on("error", (err) => {
-  console.log("Mongoose connection error: ", err);
+    console.log("Mongoose connection error: ", err);
 });
 
 // shows that backend server is listening
 app.get("/api/config", (req, res) => {
     res.json({
-      success: true,
+        success: true,
     });
-  });
+});
 
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-  });
+});
