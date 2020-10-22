6a641f76309d3e14b15d4f4931f676e8ff1a9b8f
John Alonso <jvalon9455@gmail.com>
2020-10-21 21:53:30 -0400

    created schema in workout.js models

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+const { strict } = require("assert");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String;
+                    trim: true,
+                    required: "Enter type of exercise"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter the name of the exercise"
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter duration in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                sets: {
+                    type: Number,
+                },
+                reps: {
+                    type: Number,
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    }
+)
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
