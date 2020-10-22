fe2c97d27cf1b7ba89549afe56e9b63a752e21a4
John Alonso <jvalon9455@gmail.com>
2020-10-21 21:56:58 -0400

    called in models db in server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const db = require("./models");
+
 // add express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
